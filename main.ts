557a718028b63add6dffc428d32337f0f7b95227
Arsalan Sheikh <160051126+arsalansheikh07@users.noreply.github.com>
2024-03-24 15:18:42 +0500

    commit

---
+++ main.ts
@@ -0,0 +1,45 @@
+
+import inquirer from "inquirer";
+
+// 1) computer will generate a random number - done
+
+// 2) user input for guessimg number
+
+// 3) compare user input with computer generated number and show result
+
+
+// const randomNumber = 13;
+
+// const answer = await inquirer.prompt([
+//     {
+//         name: "userGuessedNumber",
+//         type: "number",
+//         message: "Please guess a number:",
+//     },
+// ]);
+
+// if(answer.userGuessedNumber === randomNumber){
+//     console.log("Congratulation you guessed right number")
+// } else {
+//     console.log("Sorry you guessed wrong number")
+// }
+
+// example 
+
+const randomNumber = Math.floor(Math.random() * 100);                          
+
+console.log(randomNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
